bfcd21a084ae27bace967bf2c94d41b2d097dd0d
LucasAraujo1306 <lucasisaac007@gmail.com>
2023-02-01 17:34:54 -0300

    Atualização para criptografa a senha

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -1,10 +1,14 @@
 const pool = require("../connections/pg")
+const bcrypt = require('bcrypt');
 
 const cadastrarUsuario = async (req, res) => {
     const { nome, email, senha } = req.body
 
     try {
-        const novoUsuario = await pool.query('insert into usuarios (nome,email,senha) values ($1,$2,$3) returning *', [nome, email, senha]);
+        const senhaCripto = await bcrypt.hash(senha, 10)
+
+
+        const novoUsuario = await pool.query('insert into usuarios (nome,email,senha) values ($1,$2,$3) returning *', [nome, email, senhaCripto]);
 
         return res.status(201).json(novoUsuario.rows[0]);
     } catch (error) {
